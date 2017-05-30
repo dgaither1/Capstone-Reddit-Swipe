485f80e7dd9c5254eccb054c624be1d789ac12e9
David Gaither <david.gaither@usaa.com>
2017-05-30 14:17:04 -0500

    Pull down subreddits from Reddit api

---
+++ app/src/main/java/com/dg/redditswipe/LoginResultActivity.java
@@ -0,0 +1,144 @@
+package com.dg.redditswipe;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class LoginResultActivity extends AppCompatActivity {
+
+    private OkHttpClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(getIntent()!=null && getIntent().getAction().equals(Intent.ACTION_VIEW)) {
+            Uri uri = getIntent().getData();
+            if(uri.getQueryParameter("error") != null) {
+                String error = uri.getQueryParameter("error");
+                closeWithError("We had a problem logging you in to Reddit. Please try again.  Error = " + error);
+            } else {
+                String state = uri.getQueryParameter("state");
+                if(state.equals(getString(R.string.state))) {
+                    String code = uri.getQueryParameter("code");
+                    getAccessToken(code);
+                }
+            }
+        }
+    }
+
+    private void closeWithError(String errorMessage) {
+        Toast.makeText(LoginResultActivity.this, errorMessage, Toast.LENGTH_LONG).show();
+        finish();
+    }
+
+    private void getAccessToken(String code) {
+        client = new OkHttpClient();
+
+        String authString = getString(R.string.client_id) + ":";
+        String encodedAuthString = Base64.encodeToString(authString.getBytes(),
+                Base64.NO_WRAP);
+
+        Request request = new Request.Builder()
+                .addHeader("User-Agent", "RedditSwipe")
+                .addHeader("Authorization", "Basic " + encodedAuthString)
+                .url(getString(R.string.access_token_url))
+                .post(RequestBody.create(MediaType.parse("application/x-www-form-urlencoded"),
+                        "grant_type=authorization_code&code=" + code +
+                                "&redirect_uri=" + getString(R.string.redirect_uri)))
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                closeWithError("We had a problem logging you in to Reddit. Please try again");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String json = response.body().string();
+
+                JSONObject data = null;
+                try {
+                    data = new JSONObject(json);
+                    String accessToken = data.optString("access_token");
+                    String refreshToken = data.optString("refresh_token");
+
+                    getSubreddits(accessToken);
+
+                } catch (JSONException e) {
+                    closeWithError("We had a problem logging you in to Reddit. Please try again");
+                }
+            }
+        });
+    }
+
+    private void getSubreddits(String accessToken) {
+        Request request = new Request.Builder()
+                .addHeader("User-Agent", "RedditSwipe")
+                .addHeader("Authorization", "bearer " + accessToken)
+                .url(getString(R.string.subreddit_url))
+                .get()
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e("DG", "ERROR: " + e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String json = response.body().string();
+
+                Log.d("DG", json);
+
+                JSONObject jsonResponse = null;
+                try {
+                    jsonResponse = new JSONObject(json);
+                    JSONObject data = jsonResponse.optJSONObject("data");
+                    JSONArray children = data.optJSONArray("children");
+
+                    if(children.length() == 0) {
+                        closeWithError("You must be subscribed to at least 1 subreddit");
+                    }
+
+                    for(int i = 0; i < children.length(); i++) {
+                        JSONObject subreddit = children.getJSONObject(i).optJSONObject("data");
+
+                        Log.d("DG", "- Subreddit name = " + subreddit.optString("display_name"));
+                    }
+
+                } catch (JSONException e) {
+                    closeWithError("There was an error pulling the subreddits that you are subscribed to");
+                }
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/dg/redditswipe/MainActivity.java
+++ app/src/main/java/com/dg/redditswipe/MainActivity.java
@@ -1,7 +1,25 @@
 package com.dg.redditswipe;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +27,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startSignIn(View view) {
+        String url = String.format(getString(R.string.auth_url), getString(R.string.client_id), getString(R.string.state), getString(R.string.redirect_uri));
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(intent);
     }
 }
